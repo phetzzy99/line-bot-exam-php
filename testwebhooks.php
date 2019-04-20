fe3c0bcddf835d09af44a6724ab3fdfe13ede45f
triphet.pol <phetit_sru@hotmail.com>
2019-04-20 22:58:25 +0700

    Update testwebhooks.php

--- testwebhooks.php
+++ testwebhooks.php
@@ -40,6 +40,7 @@
     }
     #ตัวอย่าง Message Type "Image"
     else if($message == "รูปน้องแมว"){
+        $arrayPostData['to'] = $id;
         $image_url = "https://i.pinimg.com/originals/cc/22/d1/cc22d10d9096e70fe3dbe3be2630182b.jpg";
         $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
         $arrayPostData['messages'][0]['type'] = "image";
