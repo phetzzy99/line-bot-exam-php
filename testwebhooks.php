2691dfd50011af2f95902b71f3e4e5bb4b73ebd7
triphet.pol <phetit_sru@hotmail.com>
2019-04-20 22:09:01 +0700

    Update testwebhooks.php

--- testwebhooks.php
+++ testwebhooks.php
@@ -68,6 +68,6 @@ function replyMsg($arrayHeader,$arrayPostData){
         $result = curl_exec($ch);
         curl_close ($ch);
     }
-   echo $message->getHTTPStatus() . ' ' . $message->getRawBody();
+   echo "OK";
    exit;
 ?>
