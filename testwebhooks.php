8a8eed5b5acc7b2a8220a2a6cfdb976e5dbe2486
MacBook Pro <macbookpro@MacBooks-MacBook-Pro.local>
2019-04-21 09:35:09 +0700

    Commit

--- testwebhooks.php
+++ testwebhooks.php
@@ -1,14 +1,14 @@
 <?php
     $accessToken = 'wgWq9CMNJ5AsWmPgQMVSRaw7hK4yIwkiLWVDr/S+RH+MhUzS5Ygcye71iWRKGYNJdj08hq+1Bn9ckXzfDACsphDPJmdLSiONSmHa/VUDzoaytsA54IppqRhK0DgJJlrmqKKBAf4AiXlwOUEjQ947HQdB04t89/1O/w1cDnyilFU=';
     //copy Channel access token ตอนที่ตั้งค่ามาใส่
-    
+
     $content = file_get_contents('php://input');
     $arrayJson = json_decode($content, true);
-    
+
     $arrayHeader = array();
     $arrayHeader[] = "Content-Type: application/json";
     $arrayHeader[] = "Authorization: Bearer {$accessToken}";
-    
+
     //รับข้อความจากผู้ใช้
     $message = $arrayJson['events'][0]['message']['text'];
 
@@ -16,7 +16,7 @@
    $id = $arrayJson['events'][0]['source']['userId'];
 
     #ตัวอย่าง Message Type "Text"
-    if($message == "hi"){
+    if($message == "hello"){
         $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
         $arrayPostData['messages'][0]['type'] = "text";
         $arrayPostData['messages'][0]['text'] = "สวัสดีจ้าาา";
@@ -73,7 +73,7 @@ function replyMsg($arrayHeader,$arrayPostData){
         curl_setopt($ch, CURLOPT_URL,$strUrl);
         curl_setopt($ch, CURLOPT_HEADER, false);
         curl_setopt($ch, CURLOPT_POST, true);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $arrayHeader);    
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $arrayHeader);
         curl_setopt($ch, CURLOPT_POSTFIELDS,json_encode($arrayPostData));
         curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
